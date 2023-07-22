1323765f4346156b280815a1d326e52c0e403652
yusufemrebilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2023-07-22 20:11:51 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ void menu(void);
 int main(void){
 
     menu();
-
+    
     return 0;
 }
 
@@ -18,15 +18,18 @@ void menu(void){
     while (true) {
         
         clearscreen();
-        gotoxy(COORD_X,COORD_Y);
         header(HEADER_SIZE, 1, "UNIT CONVERTER");
-        printAtCoordinates("\xBA\xB3 Length          [1]",   gotoxy, COORD_X + 27, COORD_Y + 3);
-        printAtCoordinates("\xBA\xB3 Temperature     [2]",   gotoxy, COORD_X + 27, COORD_Y + 5);
-        printAtCoordinates("\xBA\xB3 Area            [3]",   gotoxy, COORD_X + 27, COORD_Y + 7);
-        printAtCoordinates("\xBA\xB3 Volume          [4]",   gotoxy, COORD_X + 27, COORD_Y + 9);
-        printAtCoordinates("\xBA\xB3 Mass            [5]",   gotoxy, COORD_X + 27, COORD_Y + 11);
-        printAtCoordinates("\xBA\xB3 Data            [6]",   gotoxy, COORD_X + 27, COORD_Y + 13);
-        printAtCoordinates("\xBA\xB3 Press [0] for exit.",   gotoxy, COORD_X + 27, COORD_Y + 16);
+        printAtCoordinates(COORD_X + 27, COORD_Y + 3, 8,
+            "\xBA\xB3 Length          [1]",
+            "\xBA\xB3 Temperature     [2]",
+            "\xBA\xB3 Area            [3]",
+            "\xBA\xB3 Volume          [4]",
+            "\xBA\xB3 Mass            [5]",
+            "\xBA\xB3 Data            [6]",
+            "",
+            "\xBA\xB3 Press [0] for exit."
+        );
+          
         gotoxy(COORD_X + 27, COORD_Y + 19);
 
         while (choice = getch())
@@ -37,28 +40,35 @@ void menu(void){
 
             case LENGTH:
                 clearscreen();
-                gotoxy(COORD_X,COORD_Y);
                 header(HEADER_SIZE + 4, 1, "LENGTH");
-                printAtCoordinates("\xB3 From: ", gotoxy, COORD_X + 10, COORD_Y + 3);
-                printAtCoordinates("\xB3 To:   ", gotoxy, COORD_X + 10, COORD_Y + 5);
-                printAtCoordinates("\xBA\xB3 Kilometer     [1]",  gotoxy, COORD_X + 9, COORD_Y + 8);
-                printAtCoordinates("\xBA\xB3 Meter         [2]",  gotoxy, COORD_X + 9, COORD_Y + 10);
-                printAtCoordinates("\xBA\xB3 Centimeter    [3]",  gotoxy, COORD_X + 9, COORD_Y + 12);
-                printAtCoordinates("\xBA\xB3 Milimeter     [4]",  gotoxy, COORD_X + 9, COORD_Y + 14);
-                printAtCoordinates("\xBA\xB3 Micrometer    [5]",  gotoxy, COORD_X + 9, COORD_Y + 16);
-                printAtCoordinates("\xBA\xB3 Nanometer     [6]",  gotoxy, COORD_X + 47, COORD_Y + 8);
-                printAtCoordinates("\xBA\xB3 Mile          [7]",  gotoxy, COORD_X + 47, COORD_Y + 10);
-                printAtCoordinates("\xBA\xB3 Yard          [8]",  gotoxy, COORD_X + 47, COORD_Y + 12);
-                printAtCoordinates("\xBA\xB3 Foot          [9]",  gotoxy, COORD_X + 47, COORD_Y + 14);
-                printAtCoordinates("\xBA\xB3 Inch          [10]", gotoxy, COORD_X + 47, COORD_Y + 16);
+                printAtCoordinates(COORD_X + 10, COORD_Y + 3, 2,
+                    "\xB3 From: ",
+                    "\xB3 To:   "
+                );
+                printAtCoordinates(COORD_X +9, COORD_Y + 8, 5,
+                    "\xBA\xB3 Kilometer     [1]",
+                    "\xBA\xB3 Meter         [2]",
+                    "\xBA\xB3 Centimeter    [3]",
+                    "\xBA\xB3 Milimeter     [4]",
+                    "\xBA\xB3 Micrometer    [5]"
+                );
+                printAtCoordinates(COORD_X + 47, COORD_Y + 8, 5,
+                    "\xBA\xB3 Nanometer     [6]",
+                    "\xBA\xB3 Mile          [7]",
+                    "\xBA\xB3 Yard          [8]",
+                    "\xBA\xB3 Foot          [9]",
+                    "\xBA\xB3 Inch          [10]"
+                );
                 gotoxy(COORD_X + 20, COORD_Y + 3);
-                getMenuInput(&from, 2, 10);
+                getMenuChoice(&from, 2, 10);
                 printUnitName(from, LENGTH, gotoxy, COORD_X + 20, COORD_Y + 3);
                 gotoxy(COORD_X + 20, COORD_Y + 5);
-                getMenuInput(&to,   2, 10);
+                getMenuChoice(&to,   2, 10);
                 printUnitName(to, LENGTH, gotoxy, COORD_X + 20, COORD_Y + 5);
-                printAtCoordinates("\xB3 Input:   ", gotoxy, COORD_X + 48, COORD_Y + 3);
-                printAtCoordinates("\xB3 Result:  ", gotoxy, COORD_X + 48, COORD_Y + 5);
+                printAtCoordinates(COORD_X + 48, COORD_Y + 3, 2,
+                    "\xB3 Input:   ",
+                    "\xB3 Result:  "
+                );
                 value = inputValue(gotoxy, COORD_X + 58, COORD_Y + 3);
                 result = convertLength(from, to, value);
                 printConversionResult(result, gotoxy, COORD_X + 58, COORD_Y + 5);
@@ -66,21 +76,28 @@ void menu(void){
 
             case TEMPERATURE:
                 clearscreen();
-                gotoxy(COORD_X,COORD_Y);
                 header(HEADER_SIZE + 1, 1, "TEMPERATURE");
-                printAtCoordinates("\xB3 From: ", gotoxy, COORD_X + 10, COORD_Y + 3);
-                printAtCoordinates("\xB3 To:   ", gotoxy, COORD_X + 10, COORD_Y + 5);
-                printAtCoordinates("\xBA\xB3 Celcius       [1]", gotoxy, COORD_X + 9, COORD_Y + 8);
-                printAtCoordinates("\xBA\xB3 Kelvin        [2]", gotoxy, COORD_X + 9, COORD_Y + 10);
-                printAtCoordinates("\xBA\xB3 Fahrenheit    [3]", gotoxy, COORD_X + 47, COORD_Y + 8);
+                printAtCoordinates(COORD_X + 10, COORD_Y + 3, 2,
+                    "\xB3 From: ",
+                    "\xB3 To:   "
+                );
+                printAtCoordinates(COORD_X + 9, COORD_Y + 8, 2,
+                    "\xBA\xB3 Celcius       [1]",
+                    "\xBA\xB3 Kelvin        [2]"
+                );
+                printAtCoordinates(COORD_X + 47, COORD_Y + 8, 1,
+                    "\xBA\xB3 Fahrenheit    [3]"
+                ); 
                 gotoxy(COORD_X + 19, COORD_Y + 3);
-                getMenuInput(&from, 1, 3);
+                getMenuChoice(&from, 1, 3);
                 printUnitName(from, TEMPERATURE, gotoxy, COORD_X + 19, COORD_Y + 3);
                 gotoxy(COORD_X + 19, COORD_Y + 5);
-                getMenuInput(&to  , 1, 3);
+                getMenuChoice(&to  , 1, 3);
                 printUnitName(to, TEMPERATURE, gotoxy, COORD_X + 19, COORD_Y + 5);
-                printAtCoordinates("\xB3 Input:   ", gotoxy, COORD_X + 48, COORD_Y + 3);
-                printAtCoordinates("\xB3 Result:  ", gotoxy, COORD_X + 48, COORD_Y + 5);
+                printAtCoordinates(COORD_X + 48, COORD_Y + 3, 2,
+                    "\xB3 Input:   ",
+                    "\xB3 Result:  "
+                );
                 value = inputValue(gotoxy, COORD_X + 58, COORD_Y + 3);
                 result = convertTemperature(from, to, value);
                 printConversionResult(result, gotoxy, COORD_X + 58, COORD_Y + 5);
@@ -88,28 +105,35 @@ void menu(void){
 
             case AREA:
                 clearscreen();
-                gotoxy(COORD_X,COORD_Y);
                 header(HEADER_SIZE + 5, 1, "AREA");
-                printAtCoordinates("\xB3 From: ", gotoxy, COORD_X + 5, COORD_Y + 3);
-                printAtCoordinates("\xB3 To:   ", gotoxy, COORD_X + 5, COORD_Y + 5);
-                printAtCoordinates("\xBA\xB3 Square Kilometer   [1]",  gotoxy, COORD_X + 4, COORD_Y + 8);
-                printAtCoordinates("\xBA\xB3 Square Meter       [2]",  gotoxy, COORD_X + 4, COORD_Y + 10);
-                printAtCoordinates("\xBA\xB3 Square Centimeter  [3]",  gotoxy, COORD_X + 4, COORD_Y + 12);
-                printAtCoordinates("\xBA\xB3 Square Millimeter  [4]",  gotoxy, COORD_X + 4, COORD_Y + 14);
-                printAtCoordinates("\xBA\xB3 Square Mile        [5]",  gotoxy, COORD_X + 4, COORD_Y + 16);
-                printAtCoordinates("\xBA\xB3 Square Yard        [6]",  gotoxy, COORD_X + 47, COORD_Y + 8);
-                printAtCoordinates("\xBA\xB3 Square Foot        [7]",  gotoxy, COORD_X + 47, COORD_Y + 10);
-                printAtCoordinates("\xBA\xB3 Square Inch        [8]",  gotoxy, COORD_X + 47, COORD_Y + 12);
-                printAtCoordinates("\xBA\xB3 Acre               [9]",  gotoxy, COORD_X + 47, COORD_Y + 14);
-                printAtCoordinates("\xBA\xB3 Hectare            [10]", gotoxy, COORD_X + 47, COORD_Y + 16);
+                printAtCoordinates(COORD_X + 5, COORD_Y + 3, 2,
+                    "\xB3 From: ",
+                    "\xB3 To:   "
+                );
+                printAtCoordinates(COORD_X + 4, COORD_Y + 8, 5,
+                    "\xBA\xB3 Square Kilometer   [1]",
+                    "\xBA\xB3 Square Meter       [2]",
+                    "\xBA\xB3 Square Centimeter  [3]",
+                    "\xBA\xB3 Square Millimeter  [4]",
+                    "\xBA\xB3 Square Mile        [5]"
+                );
+                printAtCoordinates(COORD_X + 47, COORD_Y + 8, 5,
+                    "\xBA\xB3 Square Yard        [6]",
+                    "\xBA\xB3 Square Foot        [7]",
+                    "\xBA\xB3 Square Inch        [8]",
+                    "\xBA\xB3 Acre               [9]",
+                    "\xBA\xB3 Hectare            [10]"
+                );
                 gotoxy(COORD_X + 15, COORD_Y + 3);
-                getMenuInput(&from, 2, 10);
+                getMenuChoice(&from, 2, 10);
                 printUnitName(from, AREA, gotoxy, COORD_X + 15, COORD_Y + 3);
                 gotoxy(COORD_X + 15, COORD_Y + 5);
-                getMenuInput(&to  , 2, 10);
+                getMenuChoice(&to  , 2, 10);
                 printUnitName(to, AREA, gotoxy, COORD_X + 15, COORD_Y + 5);
-                printAtCoordinates("\xB3 Input:   ", gotoxy, COORD_X + 48, COORD_Y + 3);
-                printAtCoordinates("\xB3 Result:  ", gotoxy, COORD_X + 48, COORD_Y + 5);
+                printAtCoordinates(COORD_X + 48, COORD_Y + 3, 2,
+                    "\xB3 Input:   ",
+                    "\xB3 Result:  "
+                );
                 value = inputValue(gotoxy, COORD_X + 58, COORD_Y + 3);
                 result = convertArea(from, to, value);
                 printConversionResult(result, gotoxy, COORD_X + 58, COORD_Y + 5);
@@ -117,28 +141,35 @@ void menu(void){
 
             case VOLUME:
                 clearscreen();
-                gotoxy(COORD_X,COORD_Y);
                 header(HEADER_SIZE + 4, 1, "VOLUME");
-                printAtCoordinates("\xB3 From: ", gotoxy, COORD_X + 5, COORD_Y + 3);
-                printAtCoordinates("\xB3 To:   ", gotoxy, COORD_X + 5, COORD_Y + 5);
-                printAtCoordinates("\xBA\xB3 US Gallon          [1]",  gotoxy, COORD_X + 4, COORD_Y + 8);
-                printAtCoordinates("\xBA\xB3 BK Gallon          [2]",  gotoxy, COORD_X + 4, COORD_Y + 10);
-                printAtCoordinates("\xBA\xB3 Liter              [3]",  gotoxy, COORD_X + 4, COORD_Y + 12);
-                printAtCoordinates("\xBA\xB3 Milliliter         [4]",  gotoxy, COORD_X + 4, COORD_Y + 14);
-                printAtCoordinates("\xBA\xB3 Cubic Centimeter   [5]",  gotoxy, COORD_X + 4, COORD_Y + 16);
-                printAtCoordinates("\xBA\xB3 Cubic Meter        [6]",  gotoxy, COORD_X + 47, COORD_Y + 8);
-                printAtCoordinates("\xBA\xB3 Cubic Mile         [7]",  gotoxy, COORD_X + 47, COORD_Y + 10);
-                printAtCoordinates("\xBA\xB3 Cubic Yard         [8]",  gotoxy, COORD_X + 47, COORD_Y + 12);
-                printAtCoordinates("\xBA\xB3 Cubic Foot         [9]",  gotoxy, COORD_X + 47, COORD_Y + 14);
-                printAtCoordinates("\xBA\xB3 Cubic Inch         [10]", gotoxy, COORD_X + 47, COORD_Y + 16);
+                printAtCoordinates(COORD_X + 5, COORD_Y + 3, 2,
+                    "\xB3 From: ",
+                    "\xB3 To:   "
+                );
+                printAtCoordinates(COORD_X + 4, COORD_Y + 8, 5,
+                    "\xBA\xB3 US Gallon          [1]",
+                    "\xBA\xB3 BK Gallon          [2]",
+                    "\xBA\xB3 Liter              [3]",
+                    "\xBA\xB3 Milliliter         [4]",
+                    "\xBA\xB3 Cubic Centimeter   [5]"
+                );
+                printAtCoordinates(COORD_X + 47, COORD_Y + 8, 5,
+                    "\xBA\xB3 Cubic Meter        [6]",
+                    "\xBA\xB3 Cubic Mile         [7]",
+                    "\xBA\xB3 Cubic Yard         [8]",
+                    "\xBA\xB3 Cubic Foot         [9]",
+                    "\xBA\xB3 Cubic Inch         [10]"
+                );
                 gotoxy(COORD_X + 15, COORD_Y + 3);
-                getMenuInput(&from, 2, 10);
+                getMenuChoice(&from, 2, 10);
                 printUnitName(from, VOLUME, gotoxy, COORD_X + 15, COORD_Y + 3);
                 gotoxy(COORD_X + 15, COORD_Y + 5);
-                getMenuInput(&to  , 2, 10);
+                getMenuChoice(&to  , 2, 10);
                 printUnitName(to, VOLUME, gotoxy, COORD_X + 15, COORD_Y + 5);
-                printAtCoordinates("\xB3 Input:   ", gotoxy, COORD_X + 48, COORD_Y + 3);
-                printAtCoordinates("\xB3 Result:  ", gotoxy, COORD_X + 48, COORD_Y + 5);
+                printAtCoordinates(COORD_X + 48, COORD_Y + 3, 2,
+                    "\xB3 Input:   ",
+                    "\xB3 Result:  "
+                );
                 value = inputValue(gotoxy, COORD_X + 58, COORD_Y + 3);
                 result = convertVolume(from, to, value);
                 printConversionResult(result, gotoxy, COORD_X + 58, COORD_Y + 5);
@@ -146,24 +177,31 @@ void menu(void){
 
             case MASS:
                 clearscreen();
-                gotoxy(COORD_X,COORD_Y);
                 header(HEADER_SIZE + 5, 1, "MASS");
-                printAtCoordinates("\xB3 From: ", gotoxy, COORD_X + 9, COORD_Y + 3);
-                printAtCoordinates("\xB3 To:   ", gotoxy, COORD_X + 9, COORD_Y + 5);
-                printAtCoordinates("\xBA\xB3 Kilogram       [1]", gotoxy, COORD_X + 8, COORD_Y + 8);
-                printAtCoordinates("\xBA\xB3 Gram           [2]", gotoxy, COORD_X + 8, COORD_Y + 10);
-                printAtCoordinates("\xBA\xB3 Milligram      [3]", gotoxy, COORD_X + 8, COORD_Y + 12);
-                printAtCoordinates("\xBA\xB3 Ton            [4]", gotoxy, COORD_X + 47, COORD_Y + 8);
-                printAtCoordinates("\xBA\xB3 Pound          [5]", gotoxy, COORD_X + 47, COORD_Y + 10);
-                printAtCoordinates("\xBA\xB3 Ounce          [6]", gotoxy, COORD_X + 47, COORD_Y + 12);
+                printAtCoordinates(COORD_X + 9, COORD_Y + 3, 2,
+                    "\xB3 From: ",
+                    "\xB3 To:   "
+                );
+                printAtCoordinates(COORD_X + 8, COORD_Y + 8, 3,
+                    "\xBA\xB3 Kilogram       [1]",
+                    "\xBA\xB3 Gram           [2]",
+                    "\xBA\xB3 Milligram      [3]"
+                );
+                printAtCoordinates(COORD_X + 47, COORD_Y + 8, 3,
+                    "\xBA\xB3 Ton            [4]",
+                    "\xBA\xB3 Pound          [5]",
+                    "\xBA\xB3 Ounce          [6]"
+                );
                 gotoxy(COORD_X + 19, COORD_Y + 3);
-                getMenuInput(&from, 1, 6);
+                getMenuChoice(&from, 1, 6);
                 printUnitName(from, MASS, gotoxy, COORD_X + 19, COORD_Y + 3);
                 gotoxy(COORD_X + 19, COORD_Y + 5);
-                getMenuInput(&to  , 1, 6);
+                getMenuChoice(&to  , 1, 6);
                 printUnitName(to, MASS, gotoxy, COORD_X + 19, COORD_Y + 5);
-                printAtCoordinates("\xB3 Input:   ", gotoxy, COORD_X + 48, COORD_Y + 3);
-                printAtCoordinates("\xB3 Result:  ", gotoxy, COORD_X + 48, COORD_Y + 5);
+                printAtCoordinates(COORD_X + 48, COORD_Y + 3, 2,
+                    "\xB3 Input:   ",
+                    "\xB3 Result:  "
+                );
                 value = inputValue(gotoxy, COORD_X + 58, COORD_Y + 3);
                 result = convertMass(from, to, value);
                 printConversionResult(result, gotoxy, COORD_X + 58, COORD_Y + 5);
@@ -171,28 +209,35 @@ void menu(void){
 
             case DATA:
                 clearscreen();
-                gotoxy(COORD_X,COORD_Y);
                 header(HEADER_SIZE + 5, 1, "DATA");
-                printAtCoordinates("\xB3 From: ", gotoxy, COORD_X + 13, COORD_Y + 3);
-                printAtCoordinates("\xB3 To:   ", gotoxy, COORD_X + 13, COORD_Y + 5);
-                printAtCoordinates("\xBA\xB3 Bit        [1]",  gotoxy, COORD_X + 12, COORD_Y + 8);
-                printAtCoordinates("\xBA\xB3 Byte       [2]",  gotoxy, COORD_X + 12, COORD_Y + 10);
-                printAtCoordinates("\xBA\xB3 Kilobit    [3]",  gotoxy, COORD_X + 12, COORD_Y + 12);
-                printAtCoordinates("\xBA\xB3 Kilobyte   [4]",  gotoxy, COORD_X + 12, COORD_Y + 14);
-                printAtCoordinates("\xBA\xB3 Megabit    [5]",  gotoxy, COORD_X + 12, COORD_Y + 16);
-                printAtCoordinates("\xBA\xB3 Megabyte   [6]",  gotoxy, COORD_X + 47, COORD_Y + 8);
-                printAtCoordinates("\xBA\xB3 Gigabit    [7]",  gotoxy, COORD_X + 47, COORD_Y + 10);
-                printAtCoordinates("\xBA\xB3 Gigabyte   [8]",  gotoxy, COORD_X + 47, COORD_Y + 12);
-                printAtCoordinates("\xBA\xB3 Terabit    [9]",  gotoxy, COORD_X + 47, COORD_Y + 14);
-                printAtCoordinates("\xBA\xB3 Terabyte   [10]", gotoxy, COORD_X + 47, COORD_Y + 16);
+                printAtCoordinates(COORD_X + 13, COORD_Y + 3, 2,
+                    "\xB3 From: ",
+                    "\xB3 To:   "
+                );
+                printAtCoordinates(COORD_X + 12, COORD_Y + 8, 5,
+                    "\xBA\xB3 Bit        [1]",
+                    "\xBA\xB3 Byte       [2]",
+                    "\xBA\xB3 Kilobit    [3]",
+                    "\xBA\xB3 Kilobyte   [4]",
+                    "\xBA\xB3 Megabit    [5]"
+                );
+                printAtCoordinates(COORD_X + 47, COORD_Y + 8, 5,
+                    "\xBA\xB3 Megabyte   [6]",
+                    "\xBA\xB3 Gigabit    [7]",
+                    "\xBA\xB3 Gigabyte   [8]",
+                    "\xBA\xB3 Terabit    [9]",
+                    "\xBA\xB3 Terabyte   [10]"
+                );
                 gotoxy(COORD_X + 23, COORD_Y + 3);
-                getMenuInput(&from, 2, 10);
+                getMenuChoice(&from, 2, 10);
                 printUnitName(from, DATA, gotoxy, COORD_X + 23, COORD_Y + 3);
                 gotoxy(COORD_X + 23, COORD_Y + 5);
-                getMenuInput(&to  , 2, 10);
+                getMenuChoice(&to  , 2, 10);
                 printUnitName(to, DATA, gotoxy, COORD_X + 23, COORD_Y + 5);
-                printAtCoordinates("\xB3 Input:   ", gotoxy, COORD_X + 48, COORD_Y + 3);
-                printAtCoordinates("\xB3 Result:  ", gotoxy, COORD_X + 48, COORD_Y + 5);
+                printAtCoordinates(COORD_X + 48, COORD_Y + 3, 2,
+                    "\xB3 Input:   ",
+                    "\xB3 Result:  "
+                );
                 value = inputValue(gotoxy, COORD_X + 58, COORD_Y + 3);
                 result = convertData(from, to, value);
                 printConversionResult(result, gotoxy, COORD_X + 58, COORD_Y + 5);
@@ -200,6 +245,8 @@ void menu(void){
 
             case EXIT:
                 clearscreen();
+                free(value);
+                free(result);
                 exit(EXIT_SUCCESS);
 
         }
