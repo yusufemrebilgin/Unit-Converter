be81c8ca5815d8660a321d000bbfb1e49f428e7e
yusufemrebilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2023-07-22 20:07:29 +0300

    Update common.h

--- header/common.h
+++ header/common.h
@@ -1,13 +1,10 @@
-#ifndef _COMMON_H
-#define _COMMON_H
-
+#ifndef     COMMON_H
+#define     COMMON_H
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
 #include <conio.h>
 #include <windows.h>
-#include <limits.h>
 
 #define COORD_X 65
 #define COORD_Y 5
@@ -15,10 +12,11 @@
 
 #define clearscreen(void) system("cls");
 
+
 void gotoxy(int x, int y);
 void header(size_t numberOfCh, unsigned int milliseconds, const char *str);
-void printAtCoordinates(const char *text, void (*coordFunction)(int,int), int x, int y);
-void getMenuInput(unsigned short *var, size_t numberOfInput, size_t numberOfOption);
+void printAtCoordinates(int coordX, int coordY, int numOfLines, ...);
+void getMenuChoice(unsigned short *var, size_t numberOfInput, size_t numberOfChoice);
 
 
 #endif
